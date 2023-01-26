aec1542934031f6679f83536a5d2922262201622
coded <55897154+TeamAbabilCoded@users.noreply.github.com>
2023-01-27 05:54:22 +0700

    Update app.js

--- app.js
+++ app.js
@@ -38,8 +38,22 @@ app.get('/', (req, res) => {
   });
 });
 
-const browser = await puppeteer.launch({
-  ignoreDefaultArgs: ['--disable-extensions'],
+const client = new Client({
+  restartOnAuthFail: true,
+  puppeteer: {
+    headless: true,
+    args: [
+      '--no-sandbox',
+      '--disable-setuid-sandbox',
+      '--disable-dev-shm-usage',
+      '--disable-accelerated-2d-canvas',
+      '--no-first-run',
+      '--no-zygote',
+      '--single-process', // <- this one doesn't works in Windows
+      '--disable-gpu'
+    ],
+  },
+  authStrategy: new LocalAuth()
 });
 
 client.on('message', msg => {
